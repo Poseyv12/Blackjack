54558da51d51556e0f787ccc5568d809d8632847
Poseyv12 <111084208+Poseyv12@users.noreply.github.com>
2023-04-24 15:54:26 -0500

    drawCard update, add calculate hand function.

--- app.js
+++ app.js
@@ -58,12 +58,14 @@ function init() {
 
 function render() {
     console.log("rendered");
+    console.log("player hand is: ", playerHand)
+    console.log("player total is: ", calculateHand(playerHand))
 }
 
 function hit() {
     console.log("hit");
     render();
-    drawCard()
+    drawCard(playerHand)
 }
 
 function stand(){
@@ -78,16 +80,48 @@ function playAgain() {
 }
 
 function bet() {
-    drawCard()
-    drawCard()
-    console.log("bet")
+    drawCard(playerHand)
+    drawCard(playerHand)
     render()
 
 }
 
-function drawCard() {
+
+function drawCard(hand) {
+    // generate a random index between 0 and the length of the deck array
     let randomIndex = Math.floor(Math.random() * deck.length);
+    // select a random card from the deck based on random index generated
     let card = deck[randomIndex];
-    console.log(`Card: ${card}`);
-    console.log(`Draw card`);
+      // add card to hand
+      hand.push(card);
+      // take card out of the deck
+    deck.splice(randomIndex, 1);
+    console.log("card dealt ", card );
 }
+
+function calculateHand(hand) {
+    let sum = 0;
+    let aces = 0;
+  
+// Retrieve the card value from the values array using the current card as the index, 
+// and store it in a variable called cardValue.
+// If the cardValue is equal to 11, increment the variable aces.
+// Add the cardValue to a running total called sum.
+    for (let i = 0; i < hand.length; i++) {
+     const cardValue = values[hand[i]];
+        if (cardValue === 11) {
+            aces++;
+        }
+        sum += cardValue;
+    }
+  
+// While there are still aces in the hand and the current sum of the hand is greater than 21:
+// Subtract 10 from the current sum.
+// Decrement the number of aces by 1.
+    while (aces > 0 && sum > 21) {
+        sum -= 10;
+        aces--;
+    }
+  return sum;
+}
+
